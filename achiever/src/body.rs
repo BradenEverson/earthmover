16f79597ff4f5c621948b2f97c7cd58cd0aea10f
Braden Everson <bradeneverson@gmail.com>
2024-09-09 10:05:02 -0500

    Add ability to grab inputs and outputs from a Peripheral Graph

--- achiever/src/body.rs
+++ achiever/src/body.rs
@@ -11,9 +11,22 @@ pub mod outputs;
 /// how we want to do this. My main idea is we can have an enum for Input/Output, and from there
 /// some sort of method for reading or writing. The a body would only be a Vec<Peripheral> but we
 /// could maybe also do a graph like structure
+#[derive(Default)]
 pub struct Body {
     /// The dependency graph of all Peripherals
     pub peripheral_graph: SlotMap<PeripheralKey, PeripheralNode>,
+}
+
+impl Body {
+    /// Returns all input nodes
+    pub fn inputs(&self) -> Vec<&PeripheralNode> {
+        self.peripheral_graph.iter().filter(|(_, node)| node.peripheral.is_input()).map(|(_, node)| node).collect::<Vec<_>>()
+    }
+
+    /// Returns all output nodes
+    pub fn outputs(&self) -> Vec<&PeripheralNode> {
+        self.peripheral_graph.iter().filter(|(_, node)| node.peripheral.is_output()).map(|(_, node)| node).collect::<Vec<_>>()
+    }
 }
 
 /// An arbitrary peripheral that is either an input or output
@@ -22,6 +35,18 @@ pub enum Peripheral {
     Input(Box<dyn Input>),
     /// Output peripheral
     Output(Box<dyn Output>),
+}
+
+impl Peripheral {
+    /// Returns true if the peripheral is an input
+    pub fn is_input(&self) -> bool {
+        matches!(self, Self::Input(_))
+    }
+
+    /// Returns true if the peripheral is an output
+    pub fn is_output(&self) -> bool {
+        matches!(self, Self::Output(_))
+    }
 }
 
 /// A node in a hardware graph that describes what peripheral it is and what it's connected in some
