1f4a7996ccda990a71c7de0bef90a55e5cff3533
BradenEverson <bradeneverson@gmail.com>
2024-09-25 05:04:05 -0500

    fix: Lints

--- earthmover-achiever/src/main.rs
+++ earthmover-achiever/src/main.rs
@@ -5,13 +5,17 @@ use std::path::PathBuf;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
+/// Configuration for the achiever session from the CLI
 struct Config {
     #[arg(short = 'b', long = "body")]
+    /// A path to the JSON file serializing the agent's body
     body: PathBuf,
     #[arg(short = 'g', long = "with-goals", value_delimiter=' ', num_args = 1..)]
+    /// An arbitrary list of usize/bool alternating pairs to create basic goals for the agent
     with_goals: Option<Vec<String>>,
     #[arg(short = 's', long = "server")]
-    server: String
+    /// An optional server to bind to
+    server: Option<String>,
 }
 
 /// Initializes an achiever system via a URDF file parsed into a `Body` and any arbitrary goals set
@@ -21,4 +25,3 @@ pub async fn main() {
     let args = Config::parse();
     println!("Todo: Parse These Args into a Body and a Goal:\n{:?}", args);
 }
-
