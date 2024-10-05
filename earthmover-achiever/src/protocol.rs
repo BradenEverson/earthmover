7ab4ce45edaf67ae192c640b3ccf5414f492c6be
Braden Everson <bradeneverson@gmail.com>
2024-10-05 17:23:09 -0500

    Ignore expr edition lint

--- earthmover-achiever/src/protocol.rs
+++ earthmover-achiever/src/protocol.rs
@@ -1,6 +1,8 @@
+//! Enum and Struct definitions for the *ArrowHead Transfer Protocol*
+
+use crate::brain::instruction::Instruction;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use uuid::Uuid;
-use crate::brain::instruction::Instruction;
 
 /// A message to send to an already initialized AHTP accepting simulation server. This must first
 /// be initialized by sending an initiation request with respect to serialized URDF and agent body
@@ -33,7 +35,8 @@ pub enum AhtpResponse {
 /// A trait to indicate that array sizes are bounded.
 pub trait ArrayBoundedSize {}
 
-// Macro to implement ArrayBoundedSize for `[f32; N]` arrays up to the specified maximum size.
+/// Macro to implement ArrayBoundedSize for `[f32; N]` arrays up to the specified maximum size.
+#[allow(edition_2024_expr_fragment_specifier)]
 macro_rules! impl_array_bounded_size {
     ($($size:expr),*) => {
         $(impl ArrayBoundedSize for [f32; $size] {} )*
@@ -41,4 +44,7 @@ macro_rules! impl_array_bounded_size {
 }
 
 // Implement ArrayBoundedSize for arrays of size 0 to 32.
-impl_array_bounded_size!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32);
+impl_array_bounded_size!(
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+    26, 27, 28, 29, 30, 31, 32
+);
