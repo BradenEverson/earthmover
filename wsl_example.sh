97a9fe8c2b17761f85464f3561df44328e1bb7a7
Braden Everson <bradeneverson@gmail.com>
2024-10-08 07:58:14 -0500

    Fix wsl_example shell script to pull from release

--- wsl_example.sh
+++ wsl_example.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cargo build -p earthmover-simulation --example physics --target x86_64-pc-windows-gnu --release &&
-cp target/x86_64-pc-windows-gnu/debug/examples/physics.exe . &&
+cp target/x86_64-pc-windows-gnu/release/examples/physics.exe . &&
 exec ./physics.exe "$@"
