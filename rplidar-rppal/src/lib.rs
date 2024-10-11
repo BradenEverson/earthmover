6e4bdafda9cef8a2054a26c4c62d568eb31a223d
BradenEverson <bradeneverson@gmail.com>
2024-10-11 05:56:09 -0500

    Add rplidar-rppal crate to workspace

---
+++ rplidar-rppal/src/lib.rs
@@ -0,0 +1,6 @@
+//! RpLidar Wrapper Functionality
+//!
+//! This crate provides wrappers aroung the RpLidar A1 module, providing a cohesive way to interact
+//! with and interpret the lidar data coming in.
+
+pub mod rplidar_a1;
