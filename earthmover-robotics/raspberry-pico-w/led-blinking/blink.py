8115a331a1078f03e42ec94db8ecea361e826247
Jawadul (Salvin) Chowdhury <sxlvin19@gmail.com>
2025-01-03 15:41:05 -0500

    embedded commit

---
+++ earthmover-robotics/raspberry-pico-w/led-blinking/blink.py
@@ -0,0 +1,14 @@
+from machine import Pin
+from utime import sleep
+
+pin = Pin("LED", Pin.OUT)
+
+print("LED starts flashing...")
+while True:
+    try:
+        pin.toggle()
+        sleep(1) # sleep 1sec
+    except KeyboardInterrupt:
+        break
+pin.off()
+print("Finished.")
