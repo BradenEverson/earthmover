aa56505e0e96466a9176cabfd46ed9527a21d393
Jawadul (Salvin) Chowdhury <sxlvin19@gmail.com>
2025-01-03 22:33:05 -0500

    led commit

--- earthmover-robotics/raspberry-pico-w-micropython/led-blinking/led.py
+++ earthmover-robotics/raspberry-pico-w-micropython/led-blinking/led.py
@@ -7,7 +7,7 @@ print("LED starts flashing...")
 while True:
     try:
         pin.toggle()
-        sleep(1) # sleep 1sec
+        sleep(10) # sleep 1sec
     except KeyboardInterrupt:
         break
 pin.off()
